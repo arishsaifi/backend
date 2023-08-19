5a33fff298d3ab2e963887588b9d4fe7359436be
[harish saifi] <harishsaifi2003@gmail.com>
2023-08-19 21:10:52 +0530

    2nd day of node

--- nynode.js
+++ nynode.js
@@ -1,10 +1,19 @@
 
 const fs = require('fs');
 
-console.log("Welcome to node ");
+var http = require('http');
 
-fs.writeFile("file.txt","hello iqra",(err) => {
-    if(err) throw err;
-    console.log('saved!');
-});
-console.log("welcome");
+// console.log("Welcome to node ");
+
+// fs.writeFile("file.txt","hello iqra",(err) => {
+//     if(err) throw err;
+//     console.log('saved!');
+// });
+// console.log("welcome");
+
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); 
